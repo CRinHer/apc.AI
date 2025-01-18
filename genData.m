59c796789d4e924a96e127f32645509c31439ef4
dy1oh <nataelpredestin@gmail.com>
2025-01-17 22:49:57 -0500

    Update genData.m

--- genData.m
+++ genData.m
@@ -1,6 +1,6 @@
-function curves = genData(list)
+function [curves, nums] = genData(list)
 %list = number of graphs you want to generate
-
+nums = []
 %set curves equal to empty struct
 curves = struct();
 for i = 1:list
@@ -8,6 +8,13 @@ for i = 1:list
     y = solveRandomModel;
     fieldName = sprintf('Graph_%d',i);
     curves.(fieldName) = y;
+
+    numsLen = length(curves.(fieldName));
+
+    nums = [nums rand(64,1)];
+
+
 end
+
 
 end
